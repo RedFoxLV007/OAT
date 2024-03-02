8374ce8a03b066a00ad63f662727ac938cec2590
REDFOX <martosb@mail.ru>
2024-03-02 14:16:54 +0300

    add l7+Cat

--- ClassWork/ОАТ организация тестового фреймворка и POM/lesson7.py
+++ ClassWork/ОАТ организация тестового фреймворка и POM/lesson7.py
@@ -18,13 +18,15 @@
 # time.sleep(5)
 
 
-#--------------------------------------------------------------------------------#
+# --------------------------------------------------------------------------------#
 from selenium import webdriver
 from selenium.common import NoSuchElementException
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 import pytest
 import time
+
+
 @pytest.fixture
 def driver():
     driver = webdriver.Chrome()
@@ -32,23 +34,24 @@ def driver():
     driver.get("https://www.python.org/search/")
     driver.implicitly_wait(5)
     yield driver
-@pytest.mark.parametrize("input_value, expected", [('math', True), ('fadsDsad',"No results found.")])
+
+
+@pytest.mark.parametrize("input_value, expected", [('math', True), ('fadsDsad', "No results found.")])
 def test_search(driver, input_value, expected):
     xpath_locator = '//*[@id="content"]/div/section/form/p/input[1]'
     class_loctor = 'list-recent-events.menu'
 
     input_elem = driver.find_element(By.XPATH, xpath_locator)
     input_elem.click()
-    input_elem.send_keys(input_value)#@pytest.mark.parametrize
+    input_elem.send_keys(input_value)  # @pytest.mark.parametrize
     input_elem.send_keys(Keys.ENTER)
     results = driver.find_element(By.CLASS_NAME, class_loctor)
     try:
-        results_li = driver.find_elements(By.CSS_SELECTOR, '.' + class_loctor + ' li' ) # '.list-recent-events menu li'
-        assert expected # @pytest.mark.parametrize
+        results_li = driver.find_elements(By.CSS_SELECTOR, '.' + class_loctor + ' li')  # '.list-recent-events menu li'
+        assert expected  # @pytest.mark.parametrize
     except NoSuchElementException:
-        results_p = driver.find_element(By.CSS_SELECTOR,'.' + class_loctor + 'p' )
-        assert results_p.text == expected #@pytest.mark.parametrize
-
+        results_p = driver.find_element(By.CSS_SELECTOR, '.' + class_loctor + 'p')
+        assert results_p.text == expected  # @pytest.mark.parametrize
 
 
 def test_empty_search(driver):
@@ -59,12 +62,11 @@ def test_empty_search(driver):
     input_elem.click()
     input_elem.send_keys('')
     input_elem.send_keys(Keys.ENTER)
-    with pytest.raises(NoSuchElementException): # я ожидаю получение исключения 'NoSuchElementException'
-        driver.find_element(By.CLASS_NAME, class_loctor) # passed test, если исключения сработало
-   # failed test если исключения не сработало
+    with pytest.raises(NoSuchElementException):  # я ожидаю получение исключения 'NoSuchElementException'
+        driver.find_element(By.CLASS_NAME, class_loctor)  # passed test, если исключения сработало
 
 
-
+# failed test если исключения не сработало
 
 
 def test_division_by_zero():
@@ -73,8 +75,9 @@ def test_division_by_zero():
     with pytest.raises(ZeroDivisionError):
         num / div
 
-@pytest.mark.parametrize("input_value, expected", [('math', True), ('fadsDsad',"No results found.")])
-def test_case_search(driver,input_value, expected):
+
+@pytest.mark.parametrize("input_value, expected", [('math', True), ('fadsDsad', "No results found.")])
+def test_case_search(driver, input_value, expected):
     class_locator = 'list-recent-events.menu'
     search = driver.find_element(By.XPATH, '//*[@id="content"]/div/section/form/p/input[1]')
     search.click()
@@ -84,7 +87,7 @@ def test_case_search(driver,input_value, expected):
     results = driver.find_element(By.CLASS_NAME, class_locator)
     try:
         results_li = driver.find_elements(By.CSS_SELECTOR, '#content > div > section > form > ul > li')
-        print('\n кол-во найденых элементов',len(results_li))
+        print('\n кол-во найденых элементов', len(results_li))
         assert expected
     except NoSuchElementException:
         results_p = driver.find_element(By.CSS_SELECTOR, '.' + class_locator + ' p')
